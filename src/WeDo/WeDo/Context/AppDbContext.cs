66362b5929d630eae2a199f25ecc1ac04f4409eb
Diego-Parreiras-Andrade <diego.andrade.1601960@sga.pucminas.br>
2026-04-27 23:03:41 -0300

    Update -Arquitetura do Codigo

---
+++ src/WeDo/WeDo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeDo.Context
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //
+            // Local para criar as DbSet para as entidades do projeto, criacao das tabelas e relacionamento entre elas
+            //
+        }
+
+    }
+}

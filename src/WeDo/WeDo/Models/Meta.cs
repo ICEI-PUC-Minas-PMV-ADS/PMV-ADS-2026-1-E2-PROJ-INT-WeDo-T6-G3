d0760b05743973d64e03b839cbce57f0b94a7ece
Diego-Parreiras-Andrade <diego.andrade.1601960@sga.pucminas.br>
2026-04-29 22:51:18 -0300

    Errata- Correção da arquitetura de contexto, e estruturação das entidades

---
+++ src/WeDo/WeDo/Models/Meta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeDo.Models
+{
+	[Table("Metas")]                                               // Nome da tabela no banco de dados
+    public class Meta
+	{
+        [Key]                                                      // Chave primária
+        public int Id { get; set; }
+        
+        public int IdUsuarioMeta { get; set; }                            // Chave estrangeira para o usuário associado à meta
+
+        [ForeignKey("IdUsuarioMeta")]                                    // Chave estrangeira para a tabela de usuários
+        public Usuario Usuario { get; set; }
+
+        [Required(ErrorMessage = "O nome da meta é obrigatório.")] // Validação de campo obrigatório
+        public string Nome { get; set; }
+
+        [Display(Name= "Descrição")]                                
+        public string Descricao { get; set; }
+
+        [Display(Name = "Data de Inicio")]                         // Exibe um rótulo personalizado para o campo
+        public DateTime DataInicial{ get; set; }
+
+        [Display(Name = "Data de Término")]
+        public DateTime DataFinal { get; set; }
+
+        public DiasDaSemana Dias { get; set; }                     // Dias Da semana para caso seja exibida diariamente
+
+        [Display(Name = "Condição")]
+        public CondicaoMeta Condicao { get; set; }                 // Condição da meta (Pendente ou Concluída)
+ 
+    }
+    public enum CondicaoMeta { Iniciada, EmAndamento, Concluida }
+    public enum DiasDaSemana { Segunda, Terca, Quarta, Quinta, Sexta, Sabado, Domingo }
+
+}
---
+++ src/WeDo/WeDo/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeDo.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio preecher seu nome")]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string UrlFoto { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio preecher seu email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio preecher sua senha")]
+        public string Senha { get; set; }
+
+        public ICollection<Meta> Metas { get; set; }                   // Relacionamento um-para-muitos com a tabela de metas
+
+    }
+}

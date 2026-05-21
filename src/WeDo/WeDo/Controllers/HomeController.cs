891626a56105f2034ff83dc514fab2a678e32e12
Diego-Parreiras-Andrade <diego.andrade.1601960@sga.pucminas.br>
2026-05-20 21:32:01 -0300

    comeco da criacao de dashboard

--- src/WeDo/WeDo/Controllers/HomeController.cs
+++ src/WeDo/WeDo/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using WeDo.Models;
-
 namespace WeDo.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            int usuarioLogado = 1; // ID fixo apenas para testes 
+            DateTime dataHoje = DateTime.Today;
+            DayOfWeek diaDaSemana = dataHoje.DayOfWeek;
+
+            var atvDiaria = _context.Metas.Where(m => m.IdUsuarioMeta == usuarioLogado                // quebrada ainda, vou continuar daqui
+                                                 && m.Condicao != CondicaoMeta.Concluida
+                                                 && dataHoje <= m.DataFinal
+                                                 && diaDaSemana == m.Condicao.CompareTo(1);
+
             return View();
         }
 

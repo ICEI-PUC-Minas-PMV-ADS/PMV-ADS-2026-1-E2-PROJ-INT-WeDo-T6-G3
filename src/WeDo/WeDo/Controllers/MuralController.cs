1a0e4511f8ed573cd1a521e9ca766d07724dff8c
Diego-Parreiras-Andrade <diego.andrade.1601960@sga.pucminas.br>
2026-06-15 22:13:25 -0300

    adicao do mural e correcao de erro de tela index ao trocar senha

---
+++ src/WeDo/WeDo/Controllers/MuralController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Linq;
+using WeDo.Models;
+
+namespace WeDo.Controllers
+{
+    [Authorize]
+    public class MuralController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MuralController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (!int.TryParse(userIdStr, out int userId)) return RedirectToAction("Login", "Usuarios");
+
+            // CORREÇÃO: Buscando pelo Enum correto (CondicaoMeta.Concluida) e pela foreign key certa (IdUsuarioMeta)
+            var conquistas = await _context.Metas
+                .Where(m => m.IdUsuarioMeta == userId && m.Condicao == CondicaoMeta.Concluida)
+                .OrderByDescending(m => m.DataFinal)
+                .ToListAsync();
+
+            return View(conquistas);
+        }
+    }
+}
---
+++ src/WeDo/WeDo/Views/Mural/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeDo.Views.Mural
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
